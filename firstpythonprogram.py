74cd10614be627f7fd9213b4fb657f261583b160
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-06 21:05:50 +0530

    Add files via upload

---
+++ firstpythonprogram.py
@@ -0,0 +1,19 @@
+#Printing statements
+print("My first python program.")
+print(5)
+print(10+10)
+print("hey",5,7,sep="~",end=" mhk\n")
+print("Mahak")
+print("hey" + "world")
+print("hey","world")
+
+"""
+Output-
+My first python program.
+5
+20
+hey~5~7 mhk
+Mahak
+heyworld
+hey world
+"""
