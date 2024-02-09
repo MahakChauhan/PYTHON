23f3088d718bfe2c272b7dad26181dc70d065016
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-09 17:43:04 +0530

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -3,6 +3,6 @@ a=input("Enter a number=") #we can take input from user with the help of input()
 print("Number is: ",a)
 a1=input("Enter first number=")
 a2=input("Enter second number=")
-print("Sum is:",int(a1)+int(a2) ) #we typecaste a1 and a2  into integer  because string is taken by input function and we have to typecaste into intger
+print("Sum is:",int(a1)+int(a2) ) #we typecaste a1 and a2  into integer  because string is taken by input function and we have to typecaste into integer
 
 
