e5e19425d39d0f48960f07a00f9e4bbfbff14ab9
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-10 19:27:39 +0530

    Update for_loop.py

--- For_loop/for_loop.py
+++ For_loop/for_loop.py
@@ -8,4 +8,25 @@ for x in colors:
  print(x)
 for i in range(5):
  print(i)
-        
+ """
+ Output=
+ A
+b
+This is something special
+h
+i
+s
+h
+e
+k
+red
+green
+blue
+yellow
+0
+1
+2
+3
+4
+ """
+           
