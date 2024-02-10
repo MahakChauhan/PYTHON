124dcec6cc7f5a7f3b57b2787e2538eab34917cd
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-10 19:22:13 +0530

    Add files via upload

---
+++ For_loop/for_loop.py
@@ -0,0 +1,11 @@
+name="Abhishek"
+for i in name:
+ print(i,sep=",")
+ if(i=='b'):
+  print("This is something special")
+colors=["red","green","blue","yellow"]
+for x in colors:
+ print(x)
+for i in range(5):
+ print(i)
+        
