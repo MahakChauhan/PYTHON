9d8ab1875c7ab0d4d9b2c882ce026e6be0811a31
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-11 16:18:01 +0530

    Add files via upload

---
+++ Functions/Function.py
@@ -0,0 +1,26 @@
+# a=6
+# b=4
+# gmean=(a*b)/(a+b)
+# print(mean)
+def gMean(a,b):
+ gmean=(a*b)/(a+b)
+ print(gmean)
+gMean(6,3)
+def isGreater(a,b):
+ if(a>b):
+  print("a is greater")
+ else:
+  print("b is greater")
+isGreater(7,5)
+def isSmaller(a,b):
+ if(a<b):
+  print("a is smaller")
+ else:
+  print("b is smaller")
+isSmaller(2,7)
+"""
+Output=
+2.0
+a is greater
+a is smaller
+"""
