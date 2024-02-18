39271cd9e5d32ea16346888335a7c5614b2d7bee
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-18 18:47:44 +0530

    Add files via upload

---
+++ 24-Day24-Dictionary-Methods/dictionary_methods.py
@@ -0,0 +1,10 @@
+dic1={1:2,45:101,34:100,3:23}
+dic2={5:25,6:36}
+# dic1.update(dic2) #update will add key-value pair of dic2 into dic1.
+# dic1.clear() # clear() will sremove all the elements from dictionary.
+# dic1.pop(45) # pop() will delete particular key-value pair.
+# dic1.popitem() #it will removes last key-value pair from dictionary.
+del dic1[1] # it will delete whole dictioanry if value is not provided like 1.
+print(dic1)
+
+
