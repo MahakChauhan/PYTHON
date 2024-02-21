ff5db356bf90d6a98762cf0e1258d665db195fcd
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-21 19:36:00 +0530

    Add files via upload

---
+++ 30-Day30-short-hand-if-else/short-hand-if-else.py
@@ -0,0 +1,5 @@
+a=int(input("Enter first number="))
+b=int(input("Enter second number="))
+print("first number is greater",a)if a>b else  print("Both are eqaual ") if  a==b else print("second number is greater",b)
+c="YEs" if a>b else 0
+print(c)
