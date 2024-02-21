86a637ccce882598feb3f8ab7e973e18cd4514c7
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-21 19:38:49 +0530

    Add files via upload

---
+++ 31-Day31-Enumerate-function/Enumerate-function.py
@@ -0,0 +1,13 @@
+marks=[20,45,60,89,20]
+# index=0
+# for mark in marks:
+#     print(mark)
+#     if(index==3):
+#         print("Mahak  Awesome")
+#     index+=1
+#using Enumerate function
+for  index ,mark in enumerate( marks):
+     print(mark)
+     if(index==3):
+         print("Mahak  Awesome")
+     
