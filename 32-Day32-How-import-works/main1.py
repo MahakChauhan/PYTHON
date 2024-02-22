44419736b00a930cffa5bd253c7119f0bd1bc285
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-22 20:36:21 +0530

    Add files via upload

---
+++ 32-Day32-How-import-works/main1.py
@@ -0,0 +1,14 @@
+import math
+# from math import sqrt
+#  result=sqrt(17)
+# from math import *
+# from math import sqrt as s
+# result=s(17)
+# print(result)
+print(dir(math))
+# from mahak import *
+# print(Mahak)
+# fun()
+import mahak as m
+print(m.Mahak)
+m.fun()
