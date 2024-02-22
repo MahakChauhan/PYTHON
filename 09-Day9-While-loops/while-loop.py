c867c83de9c097e6fbe0c57d6b17775527682d87
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-22 19:45:39 +0530

    Add files via upload

---
+++ 09-Day9-While-loops/while-loop.py
@@ -0,0 +1,17 @@
+i=0
+#works until the condition will be false
+while(i<3):
+    print(i)
+    i=i+1 #increment by 1
+i=int(input("Enter a number="))
+while(i<20):
+    i=int(input("Enter a number="))
+    print(i)   
+print("Done with the loop")    
+#else with while
+i=5
+while(i>0): 
+    print(i)   
+    i=i-1 ##decremented by 1
+else:
+ print("i am inside else")    
