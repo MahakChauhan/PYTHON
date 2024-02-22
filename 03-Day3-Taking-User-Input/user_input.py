a8e3aee91d64d4da0b47d3e4e6d42fd6605d6b10
MahakChauhan <145031691+MahakChauhan@users.noreply.github.com>
2024-02-22 19:24:31 +0530

    Add files via upload

---
+++ 03-Day3-Taking-User-Input/user_input.py
@@ -0,0 +1,8 @@
+#how to take  input  from user
+a=input("Enter a number=") #we can take input from user with the help of input() function
+print("Number is: ",a)
+a1=input("Enter first number=")
+a2=input("Enter second number=")
+print("Sum is:",int(a1)+int(a2) ) #we typecaste a1 and a2  into integer  because string is taken by input function and we have to typecaste into intger
+
+
